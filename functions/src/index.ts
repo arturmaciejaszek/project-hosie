6f94f9efbe80547fdbb3a6ccc01f22fd9e187cad
ImSane2 <a.maciejaszek@gmail.com>
2018-03-13 21:56:38 +0100

    thumnail cloud function

---
+++ functions/src/index.ts
@@ -0,0 +1,71 @@
+import * as functions from 'firebase-functions';
+import * as gStorage from '@google-cloud/storage';
+import * as path from 'path';
+import * as os from 'os';
+import * as sharp from 'sharp';
+
+const gcs = gStorage();
+
+const THUMB_MAX_WIDTH = 200;
+const THUMB_MAX_HEIGHT = 200;
+
+export const thumbnail = functions.storage.object().onChange( event => {
+    const object = event.data;
+    const fileBucket = object.bucket;
+    const contentType = object.contentType;
+    const resourceState = object.resourceState;
+    const metageneration = object.metageneration;
+    const filePath = object.name;
+    console.log('I see the change');
+
+    if (!contentType.startsWith('image/')) {
+        console.log('This is not an image.');
+        return null;
+    }
+
+    if (path.basename(filePath).startsWith('thumb_')) {
+        console.log('already a thumbnail');
+        return null;
+    }
+
+    if (!path.basename(filePath).startsWith('toThumb_')) {
+        console.log('not meant for thumbnail');
+        return null;
+    }
+
+    if (resourceState === 'not_exists') {
+        console.log('This is a deletion event.');
+        return null;
+    }
+
+    if (resourceState === 'exists' && metageneration > 1) {
+        console.log('This is a metadata change event.');
+        return null;
+    }
+
+    const bucket = gcs.bucket(fileBucket);
+    const filename = path.basename(filePath);
+    const metadata = { contentType: contentType };
+    const pipeline = sharp();
+
+    const thumbFileName = `thumb_${filename}`;
+    const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
+    const thumbnailUploadStream = bucket.file(thumbFilePath).createWriteStream({metadata});
+
+    pipeline
+        .resize(THUMB_MAX_WIDTH, THUMB_MAX_HEIGHT)
+        .max()
+        .pipe(thumbnailUploadStream);
+
+    bucket.file(filePath).createReadStream().pipe(pipeline);
+
+    const streamAsPromise = new Promise((resolve, reject) =>
+        thumbnailUploadStream.on('finish', resolve).on('error', reject));
+
+    return streamAsPromise.then(() => {
+        bucket.file(filePath).delete().then(_ => console.log('original file deleted')).catch(err => console.log(err));
+        console.log('Thumbnail created successfully');
+        return null;
+    });
+
+});
